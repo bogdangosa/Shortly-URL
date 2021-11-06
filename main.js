b7de3c837d40f78ca32776d5ca2e2b5c095615a1
bogdangosa <57357871+bogdangosa@users.noreply.github.com>
2021-11-06 14:47:06 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const ShortenBtn = document.getElementById("shorten-btn");
+const LinkInput = document.getElementById("link-input");
+
+
+ShortenBtn.addEventListener('click',()=>{
+    console.log(LinkInput.value);
+})
